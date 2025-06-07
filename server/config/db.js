bcb8aabd37eded39fd1fbbd1bd635728749b14c8
canerinali <161130051@istanbularel.edu.tr>
2025-06-08 01:17:03 +0300

    Veritabanı yapılandırmasında 'database' anahtarının ismi 'DB_NAME' yerine 'DB_DATABASE' olarak güncellendi.

--- server/config/db.js
+++ server/config/db.js
@@ -5,7 +5,7 @@ const config = {
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     server: process.env.DB_HOST,
-    database: process.env.DB_NAME,
+    database: process.env.DB_DATABASE,
     options: {
         encrypt: true, // Azure'da çalıştırıyorsanız true yapın
         trustServerCertificate: true // Localhost veya güvenilir olmayan sertifika için true
